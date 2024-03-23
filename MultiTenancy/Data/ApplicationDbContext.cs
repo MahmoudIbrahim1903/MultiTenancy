37960e8fabdfc627e0b0dfec50f99edbad59068c
AzureAD\MahmoudIbrahimAli <mahmoud.ali@grinta.ai>
2024-03-23 02:31:48 +0200

    packages installation

---
+++ MultiTenancy/Data/ApplicationDbContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MultiTenancy.Services;
+
+namespace MultiTenancy.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public string TenantId { get; set; }
+        private readonly ITenantService _tenantService;
+        public ApplicationDbContext(DbContextOptions options, ITenantService tenantService) : base(options)
+        {
+            _tenantService = tenantService;
+            TenantId = tenantService.GetCurrentTenant().TenantId
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = _tenantService.GetConnectionString();
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                string dbProvider = _tenantService.GetDatabaseProvider();
+
+                if (dbProvider?.ToLower() == "mssql")
+                {
+                    optionsBuilder.UseSqlServer(connectionString);
+                }
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BaseEntity>().HasQueryFilter(e => e.TenantId == TenantId);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>().Where(e => e.State == EntityState.Added))
+            {
+                entry.Entity.TenantId = TenantId;
+            }
+            return base.SaveChanges();
+        }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseEntity>().Where(e => e.State == EntityState.Added))
+            {
+                entry.Entity.TenantId = TenantId;
+            }
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
